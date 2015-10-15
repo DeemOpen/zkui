8451f97e0cd137a7a183d4bede6d59ccada2608c
yuanyue <87439247@qq.com>
2015-10-15 23:21:22 +0800

    session 超时时间为1小时

--- src/main/java/com/deem/zkui/Main.java
+++ src/main/java/com/deem/zkui/Main.java
@@ -1,27 +1,27 @@
 /**
- *
  * Copyright (c) 2014, Deem Inc. All Rights Reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- *
  */
 package com.deem.zkui;
 
 import com.deem.zkui.dao.Dao;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Date;
 import java.util.Properties;
+
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.server.Connector;
@@ -64,6 +64,11 @@ public class Main {
         Server server = new Server();
 
         WebAppContext servletContextHandler = new WebAppContext();
+
+        /*****/
+        servletContextHandler.getSessionHandler().getSessionManager().setMaxInactiveInterval(3600);
+        /*****/
+
         servletContextHandler.setContextPath("/");
         servletContextHandler.setResourceBase("src/main/resources/" + webFolder);
         ClassList clist = ClassList.setServerDefault(server);
@@ -86,7 +91,7 @@ public class Main {
         HttpConfiguration http_config = new HttpConfiguration();
         http_config.setSecureScheme("https");
         http_config.setSecurePort(Integer.parseInt(globalProps.getProperty("serverPort")));
-        
+
         if (globalProps.getProperty("https").equals("true")) {
             File keystoreFile = new File(globalProps.getProperty("keystoreFile"));
             SslContextFactory sslContextFactory = new SslContextFactory();
